01e86fa1c536785054c75028914f382762fd2c9d
Akshat Maheshwari <maheshwariakshat482@gmail.com>
2023-03-25 01:04:32 +0530

    initial layout

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,42 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './layouts/header/header.component';
+import { FooterComponent } from './layouts/footer/footer.component';
+import { HomeComponent } from './pages/home/home.component';
+import { AboutUsComponent } from './pages/about-us/about-us.component';
+import { ContactUsComponent } from './pages/contact-us/contact-us.component';
+import { SubscriptionFormComponent } from './subscription-form/subscription-form.component';
+import { PostCardComponent } from './layouts/post-card/post-card.component';
+import { TestimonialsComponent } from './testimonials/testimonials.component';
+import { CommentFormComponent } from './comments/comment-form/comment-form.component';
+import { CommentListComponent } from './comments/comment-list/comment-list.component';
+import { NavbarComponent } from './layouts/navbar/navbar.component';
+import { BlogComponent } from './pages/blog/blog.component';
+import { SingleCategoryComponent } from './pages/single-category/single-category.component';
+import { SinglePostComponent } from './pages/single-post/single-post.component';
+import { TermsAndConditionsComponent } from './pages/terms-and-conditions/terms-and-conditions.component';
+import { WhoAreWeComponent } from './pages/who-are-we/who-are-we.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    HomeComponent,
+    AboutUsComponent,
+    ContactUsComponent,
+    SubscriptionFormComponent,
+    PostCardComponent,
+    TestimonialsComponent,
+    CommentFormComponent,
+    CommentListComponent,
+    NavbarComponent,
+    BlogComponent,
+    SingleCategoryComponent,
+    SinglePostComponent,
+    TermsAndConditionsComponent,
+    WhoAreWeComponent
   ],
   imports: [
     BrowserModule,
